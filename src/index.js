f4f2297b64bd60de9d237f90506ce264dcea526d
Zajahri <zajahrisolar10@gmail.com>
2024-10-02 13:06:45 +0800

    Initial commit of my Quizzy project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './part-1/App';
+import App from './todo-list-app/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
